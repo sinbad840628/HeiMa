588aec33068dcbddb49ac19e6a0a71d69ed9cb51
sinbad840628 <sinbad840628@gmail.com>
2013-08-17 21:17:14 +0800

    First Commit for Testing
    Signed-off-by: sinbad840628 <sinbad840628@gmail.com>

---
+++ src/com/java/basic/day21/JavaUtilsDemo.java
@@ -0,0 +1,32 @@
+/**   
+* @Title: JavaUtilsDemo.java 
+* @Package com.java.basic.day21 
+* @Description: TODO(用一句话描述该文件做什么) 
+* @author Sinbad sinbad840628@gmail.com   
+* @date 2013-8-15 下午11:40:12 
+* @version V1.0   
+*/
+package com.java.basic.day21;
+
+/** 
+ * @ClassName: JavaUtilsDemo 
+ * @Description: TODO(这里用一句话描述这个类的作用) 
+ * @author Sinbad sinbad840628@gmail.com 
+ * @date 2013-8-15 下午11:40:12 
+ *  
+ */
+public class JavaUtilsDemo {
+
+	/** 
+	 * @Title: main 
+	 * @Description: TODO(这里用一句话描述这个方法的作用) 
+	 * @param @param args    设定文件 
+	 * @return void    返回类型 
+	 * @throws 
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Testing,Hello~!");
+	}
+
+}
